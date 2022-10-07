f91491d6a9431dc76ac52b5aad4f5078a6a1b9ab
PierceVDR <piercevdr@gmail.com>
2022-10-07 12:13:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //BabyParrot baby = new BabyParrot("Toucan", 3,4.25);
+        //baby.setName("Lil' Beans");
+        //String info = baby.parrotInfo(); // store returned string in variable
+        //System.out.println(info);        // print the returned string
+
+        Student student = new Student("Abby", "Smith", 2023);
+        student.addTestScore(95);
+        student.addTestScore(98);
+        System.out.println(student.getStudentInfo());
+    }
+}
